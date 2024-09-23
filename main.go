4071724a52ebdb2f74d2c483b7572b2b85e1acdf
gonew11 <awork1080@gmail.com>
2024-09-23 19:04:14 +0330

    project created

--- main.go
+++ main.go
@@ -24,18 +24,3 @@ func main() {
 	fmt.Printf("Output: %s\n", output)
 
 }
-
-// func bash() {
-// 	// Define the Bash command you want to run
-// 	cmd := exec.Command("bash", "-c", "ggospider ", target)
-
-// 	// Run the command and capture its output
-// 	output, err := cmd.Output()
-// 	if err != nil {
-// 		fmt.Printf("Error: %v\n", err)
-// 		return
-// 	}
-
-// 	// Print the output
-// 	fmt.Printf("Output: %s\n", output)
-// }
